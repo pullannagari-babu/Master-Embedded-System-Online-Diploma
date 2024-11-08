d629f735b61f51e1aa766c2dffec384b9e6a9859
pullannagari babu <65277372+pullannagari-babu@users.noreply.github.com>
2024-11-08 13:12:48 +0530

    Update main_9.c

--- C Codes Reference/main_9.c
+++ C Codes Reference/main_9.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// this logic will print the 0 is front
 char *print_binary(int num)
 {
     int i=31;
@@ -17,3 +17,30 @@ int main()
     print_binary(num);
  return 0;
 }
+/*
+//another logic 
+#include <stdio.h>
+
+void printBinary(unsigned int num) {
+    int started = 0;  // To skip leading zeros
+    for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
+        if ((num >> i) & 1) {
+            started = 1;  // Start printing once we find the first '1'
+            putchar('1');
+        } else if (started) {
+            putchar('0');
+        }
+    }
+    if (!started) {
+        putchar('0');  // If num is 0, print "0"
+    }
+    putchar('\n');
+}
+
+int main() {
+    unsigned int num = 18;
+    printBinary(num);
+    return 0;
+}
+
+*/
